ebca751f3d9c993b3e9d93517a52edbc642a85c4
weiglszonja <wszonja@gmail.com>
2023-01-26 16:02:09 +0100

    black

--- src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencebehaviorinterface.py
+++ src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencebehaviorinterface.py
@@ -3,6 +3,7 @@ from pynwb.file import NWBFile
 
 from neuroconv.basedatainterface import BaseDataInterface
 
+
 class NeurotensinValenceBehaviorInterface(BaseDataInterface):
     """Behavior interface for neurotensin_valence conversion"""
 
@@ -13,10 +14,10 @@ class NeurotensinValenceBehaviorInterface(BaseDataInterface):
     def get_metadata(self):
         # Automatically retrieve as much metadata as possible
         metadata = super().get_metadata()
-        
+
         return metadata
 
     def run_conversion(self, nwbfile: NWBFile, metadata: dict):
         # All the custom code to write to PyNWB
-        
+
         return nwbfile
