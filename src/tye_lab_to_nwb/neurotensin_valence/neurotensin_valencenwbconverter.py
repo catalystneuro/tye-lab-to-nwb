35d93613fd5d83f0f49ca42bad14db7fb7ba42d2
weiglszonja <wszonja@gmail.com>
2023-02-07 20:25:20 +0100

    update converter

--- src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencenwbconverter.py
+++ src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencenwbconverter.py
@@ -1,20 +1,18 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
-from neuroconv.datainterfaces import (
-    SpikeGLXRecordingInterface,
-    SpikeGLXLFPInterface,
-    PhySortingInterface,
-)
 
-from tye_lab_to_nwb.neurotensin_valence import NeurotensinValenceBehaviorInterface
+from tye_lab_to_nwb.neurotensin_valence.behavior import NeurotensinDeepLabCutInterface
 
 
 class NeurotensinValenceNWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
-        Recording=SpikeGLXRecordingInterface,
-        LFP=SpikeGLXLFPInterface,
-        Sorting=PhySortingInterface,
-        Behavior=NeurotensinValenceBehaviorInterface,
+        Behavior=NeurotensinDeepLabCutInterface,
     )
+
+    def get_metadata_schema(self) -> dict:
+        metadata_schema = super().get_metadata_schema()
+        # Manual override to allow additional properties for pose estimation metadata
+        metadata_schema["additionalProperties"] = True
+        return metadata_schema
