d100c23307378b69757ef9f08c6e62354dc10fad
weiglszonja <wszonja@gmail.com>
2023-03-19 22:08:26 +0100

    add ecephys and sorting interfaces

--- src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencenwbconverter.py
+++ src/tye_lab_to_nwb/neurotensin_valence/neurotensin_valencenwbconverter.py
@@ -1,5 +1,9 @@
 """Primary NWBConverter class for this dataset."""
 from neuroconv import NWBConverter
+from neuroconv.datainterfaces import (
+    OpenEphysRecordingInterface,
+    PlexonSortingInterface,
+)
 
 from tye_lab_to_nwb.neurotensin_valence.behavior import NeurotensinDeepLabCutInterface
 
@@ -8,6 +12,8 @@ class NeurotensinValenceNWBConverter(NWBConverter):
     """Primary conversion class for my extracellular electrophysiology dataset."""
 
     data_interface_classes = dict(
+        Recording=OpenEphysRecordingInterface,
+        Sorting=PlexonSortingInterface,
         Behavior=NeurotensinDeepLabCutInterface,
     )
 
@@ -16,3 +22,17 @@ class NeurotensinValenceNWBConverter(NWBConverter):
         # Manual override to allow additional properties for pose estimation metadata
         metadata_schema["additionalProperties"] = True
         return metadata_schema
+
+    def get_metadata(self):
+        """Auto-fill as much of the metadata as possible. Must comply with metadata schema."""
+        metadata = super().get_metadata()
+        start_times = []
+        for interface in self.data_interface_objects.values():
+            interface_metadata = interface.get_metadata()
+            if "session_start_time" in interface_metadata["NWBFile"]:
+                start_times.append(interface_metadata["NWBFile"]["session_start_time"])
+
+        # Use the earliest session_start_time
+        metadata["NWBFile"].update(session_start_time=min(start_times))
+
+        return metadata
