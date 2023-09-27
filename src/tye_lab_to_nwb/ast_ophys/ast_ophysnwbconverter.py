fe2c86b4017b17799ed5c6e4d7953e5206e40a3d
weiglszonja <wszonja@gmail.com>
2023-09-27 11:38:52 +0200

    use the imaging or segmentation sampling freq for the processed data

--- src/tye_lab_to_nwb/ast_ophys/ast_ophysnwbconverter.py
+++ src/tye_lab_to_nwb/ast_ophys/ast_ophysnwbconverter.py
@@ -31,20 +31,26 @@ class AStOphysNWBConverter(NWBConverter):
         overwrite: bool = False,
         conversion_options: Optional[dict] = None,
     ):
-        # TODO: remove this part if
-        # if "RawImaging" in self.data_interface_objects:
-        #     miniscope_converter = self.data_interface_objects["RawImaging"]
-        #     imaging_interface = miniscope_converter.data_interface_objects["MiniscopeImaging"]
+        # Setting the sampling frequency of the processed and motion corrected imaging extractors to
+        # the raw imaging. When the raw imaging is missing but the segmentation is available, use that.
+        sampling_frequency = None
+        if "RawImaging" in self.data_interface_objects:
+            miniscope_converter = self.data_interface_objects["RawImaging"]
+            imaging_interface = miniscope_converter.data_interface_objects["MiniscopeImaging"]
+            sampling_frequency = imaging_interface.imaging_extractor.get_sampling_frequency()
+
+        elif "Segmentation" in self.data_interface_objects:
+            segmentation_interface = self.data_interface_objects["Segmentation"]
+            sampling_frequency = segmentation_interface.segmentation_extractor.get_sampling_frequency()
 
         if "MotionCorrectedImaging" in self.data_interface_objects:
             motion_corrected_imaging_interface = self.data_interface_objects["MotionCorrectedImaging"]
             # TODO: clarify rate (segmentation data and raw imaging is fs 15, but opencv frame rate for processed is 30.0)
-            motion_corrected_imaging_interface.imaging_extractor._sampling_frequency = 30.0
+            motion_corrected_imaging_interface.imaging_extractor._sampling_frequency = sampling_frequency
 
         if "ProcessedImaging" in self.data_interface_objects:
             processed_imaging_interface = self.data_interface_objects["ProcessedImaging"]
-            # TODO: clarify rate
-            processed_imaging_interface.imaging_extractor._sampling_frequency = 30.0
+            processed_imaging_interface.imaging_extractor._sampling_frequency = sampling_frequency
 
         # Update imaging plane location
         imaging_plane_metadata = metadata["Ophys"]["ImagingPlane"][0]
